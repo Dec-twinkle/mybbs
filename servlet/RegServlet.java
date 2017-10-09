f1b84ba25b22beecde70cbc316f56068a36b60c3
javapoor <31696416+javapoor@users.noreply.github.com>
2017-10-10 00:13:25 +0800

    Create RegServlet.java

---
+++ servlet/RegServlet.java
@@ -0,0 +1,71 @@
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dao.UserDao;
+import dao.UserDaoImpl;
+import domain.User;
+
+public class RegServlet extends HttpServlet {
+
+	/**
+	 * The doGet method of the servlet. <br>
+	 *
+	 * This method is called when a form has its tag value method equals to get.
+	 * 
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\">");
+		out.println("<HTML>");
+		out.println("  <HEAD><TITLE>A Servlet</TITLE></HEAD>");
+		out.println("  <BODY>");
+		out.print("    This is ");
+		out.print(this.getClass());
+		out.println(", using the GET method");
+		out.println("  </BODY>");
+		out.println("</HTML>");
+		out.flush();
+		out.close();
+	}
+
+	/**
+	 * The doPost method of the servlet. <br>
+	 *
+	 * This method is called when a form has its tag value method equals to post.
+	 * 
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		String name=request.getParameter("name");
+		String password=request.getParameter("password");
+		String email=request.getParameter("email");
+		String phone=request.getParameter("phone");
+		String school=request.getParameter("school");
+		String address=request.getParameter("address");
+		User user = new User(0,name,password,email,phone,address,school);
+		System.out.println(user);
+		UserDao dao =new UserDaoImpl();
+		dao.saveUser(user);
+		response.sendRedirect(request.getContextPath()+"/pages/bootstrap/login.jsp");
+	}
+
+}
